e0cf4c0a9b74c7d48b5500e743e201842616ebf1
Xingbo.Jie <jiexingbo@mozat.com>
2017-03-23 15:27:48 +0800

    + structure

---
+++ app/src/main/java/com/simple_jie/codememory/App.java
@@ -0,0 +1,14 @@
+package com.simple_jie.codememory;
+
+import android.app.Application;
+
+/**
+ * Created by Xingbo.Jie on 21/3/17.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
